8a62f1b77b7e9fab050b344350d1b86623cd55bc
imosudi <imosudi@gmail.com>
2017-12-04 07:34:34 +0800

    Initial commit

---
+++ note_app/main.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, redirect, request
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+from flask import Flask
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+
+bootstrap = Bootstrap(app)
+#moment = Moment(app)
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
+db = SQLAlchemy(app)
+
+
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80))
+    body = db.Column(db.Text)
+
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/notes/create", methods=["GET", "POST"])
+def create_note():
+    if request.method == "GET":
+        return render_template("create_note.html")
+    else:
+        title = request.form["title"]
+        body = request.form["body"]
+        note = Note(title=title, body=body)
+        db.session.add(note)
+        db.session.commit()
+        return redirect("/notes/create")
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
+
+
+
